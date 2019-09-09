6720a1b2039bc6a77f3926c74dc68df57fff10b3
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2019-09-09 10:54:02 +0600

    Learn Kotlin for TBL

---
+++ src/InnerClass.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+
+    // create object for inner class call from inside of outer class
+    var INCLASS = OuterClass().Inner()
+    INCLASS.findSum()
+}
+
+class OuterClass
+{
+    var x = 40
+    var y = 20
+
+    // we cant access x & y from outer class in inner class function
+    // without writing inner keyword in front of inner class
+    inner class Inner
+    {
+        fun findSum()
+        {
+            var sum = x + y
+            println("Sum is $sum")
+        }
+
+    }
+}
