c2e6f72d32f572a665b3133ae4c2f7cac882d902
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2019-09-09 13:05:19 +0600

    Learn Kotlin for TBL

---
+++ src/MethodOverride.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+
+    // call show message function instead of Parent class
+    // truely inherited by ChildClass
+    ChildClass().showMessage()
+
+}
+// use open for availability to inheritance of this class
+open class ParentClass{
+    // Due to write open keyword here it will be convert as non final method
+    // It mean Any Child class can override this show message method
+    open fun showMessage(){
+        println("Hello From Parent Class Method")
+    }
+}
+
+// inherit by parentClass
+class  ChildClass:ParentClass()
+{
+    // this is called Method Overriding
+    override fun showMessage() {
+        println("Hello From Child Class Method")
+    }
+
+}
