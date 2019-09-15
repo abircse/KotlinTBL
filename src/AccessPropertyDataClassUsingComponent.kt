41ddc8c0e36058016a0fba8b14c7af2e114987f6
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2019-09-15 14:51:23 +0600

    Learn Kotlin for TBL

--- src/AccessPropertyDataClassUsingComponent.kt
+++ src/AccessPropertyDataClassUsingComponent.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
 
     val p1 = Product("Laptop",20000)
-    val p2 = Product("Kayboard",20000)
+    val p2 = Product("Keyboard",20000)
 
     println("Product 1, Name: ${p1.component1()}  Price: ${p1.component2()}")
     println("Product 2, Name: ${p2.component1()}  Price: ${p2.component2()}")
